0817c7d867a54c282a10349948f72ef49ab1c2a2
galaterro <adrix1992@gmail.com>
2016-12-17 20:14:52 +0100

    Initial commit

---
+++ src/es/adrigala/naves/controller/Main.java
@@ -0,0 +1,12 @@
+package es.adrigala.naves.controller;
+
+import es.adrigala.naves.view.JFPantalla;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        JFPantalla jfp = new JFPantalla();
+
+    }
+}
---
+++ src/es/adrigala/naves/view/JFPantalla.java
@@ -0,0 +1,7 @@
+package es.adrigala.naves.view;
+
+/**
+ * Created by Adri on 17/12/2016.
+ */
+public class JFPantalla {
+}
