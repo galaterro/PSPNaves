7e2b64de59a701e174a54d066616f36115012544
galaterro <adrix1992@gmail.com>
2016-12-19 09:32:35 +0100

    deleted gui

--- src/es/adrigala/naves/controller/Main.java
+++ src/es/adrigala/naves/controller/Main.java
@@ -1,12 +1,9 @@
 package es.adrigala.naves.controller;
 
-import es.adrigala.naves.view.JFPantalla;
-
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        JFPantalla jfp = new JFPantalla();
 
     }
 }
